813e4ce6747c122a43e233e77fd1fa28af62600c
tulinakbulut <akbuluttulin@gamil.com>
2018-02-06 06:23:25 -0500

    Iinitial checked and added close method

---
+++ BasicTest/src/BasicTest/BasicSearch.java
@@ -0,0 +1,29 @@
+package BasicTest;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class BasicSearch {
+	@Test
+		public void search(){
+		System.setProperty("webdriver.chrome.driver", "/Users/tulinakbulut/Documents/libraries/selenium-java-3/chromedriver");
+
+		WebDriver driver=new ChromeDriver();
+        String url="https://amazon.com";
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(url);
+        driver.findElement(By.id("twotabsearchtextbox")).sendKeys("iphone 6s"+Keys.ENTER);
+        String element=driver.findElement(By.id("s-result-count")).getText();
+        System.out.println(element);
+       driver.quit();
+}
+
+}
+
